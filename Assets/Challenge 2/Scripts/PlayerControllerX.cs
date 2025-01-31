e5e38e74b3ffc5c954e6baf951ffef1af29bf06c
Oleg <oleg19994000@gmail.com>
2025-01-31 20:40:25 +0300

    Прототип 2

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float timeRemaining = 0.09f;
+    private float timer;
+
+    private void Start()
+    {
+        timer = timeRemaining;
+    }
+    // Update is called once per frame
+
+    void Update()
+    {
+        // On spacebar press, send dog
+
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            timer = timeRemaining;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            }
+        }
+
+    }
+}
